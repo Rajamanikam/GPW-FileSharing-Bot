d2e75117471b4916fc3763a92546c589a03dcbcd
NeonAnurag <justasneon@gmail.com>
2025-05-01 17:51:45 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -11,7 +11,7 @@ from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("<b>Please Wait...!</b>", quote = True)
+    reply_text = await message.reply_text("<b><i>Please Wait...!</i></b>", quote = True)
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:
@@ -19,7 +19,7 @@ async def channel_post(client: Client, message: Message):
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
-        await reply_text.edit_text("<b>Something Went Wrong..!</b>")
+        await reply_text.edit_text("<b><i>Something Went Wrong..!</i></b>")
         return
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
