6cc55333a322997844e93e11dfdab4a2c9080b8b
Rajamanikam <52708424+Rajamanikam@users.noreply.github.com>
2025-05-23 18:14:51 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,11 +4,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("NeonFiles")
-
-
-
-
-# MyselfNeon
-# Don't Remove Credit 🥺
-# Telegram Channel @NeonFiles
+    return web.json_response({"status": "ok", "service": "Genie Projects World File Bot"})
