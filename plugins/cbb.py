bf61becb37df22cf172317487da5442c157ce639
Rajamanikam <52708424+Rajamanikam@users.noreply.github.com>
2025-05-23 18:03:09 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -4,19 +4,23 @@ from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 
-
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 <i>My Name :</i></b> <a href='https://t.me/NeonFilesBot'><b><i>NeonFilesBot</i></b></a> \n<b>📝 <i>Language :</i></b> <a href='https://python.org'><b><i>Python 3</i></b></a> \n<b>📚 <i>Library :</i></b> <a href='https://pyrogram.org'><b><i>Pyrogram {__version__}</i></b></a> \n<b>🚀 <i>Server :</i></b> <a href='https://heroku.com'><b><i>Heroku</i></b></a> \n<b>📢 <i>Channel :</i></b> <a href='https://t.me/NeonFiles'><b><i>NeonFiles</i></b></a> \n<b>🧑‍💻 <i>Developer :</i></b> <a href='tg://user?id={OWNER_ID}'><b><i>NeonAnurag</i></b></a>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+            text = (
+                f"<b>🤖 <i>Bot Name:</i></b> <a href='https://t.me/GenieSourceCodeBot'><b><i>GenieSourceCodeBot</i></b></a>\n"
+                f"<b>📝 <i>Language:</i></b> <a href='https://python.org'><b><i>Python 3</i></b></a>\n"
+                f"<b>📚 <i>Library:</i></b> <a href='https://pyrogram.org'><b><i>Pyrogram {__version__}</i></b></a>\n"
+                f"<b>🚀 <i>Server:</i></b> <a href='https://railway.app'><b><i>Railway</i></b></a>\n"
+                f"<b>📢 <i>Channel:</i></b> <a href='https://t.me/GenieProjectsWorld'><b><i>Genie Projects World</i></b></a>\n"
+                f"<b>🧑‍💻 <i>Developer:</i></b> <a href='tg://user?id={OWNER_ID}'><b><i>Rajamanikam</i></b></a>"
+            ),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
-                    [
-                        InlineKeyboardButton("🔒 Cʟᴏꜱᴇ", callback_data = "close")
-                    ]
+                    [InlineKeyboardButton("🔒 Cʟᴏꜱᴇ", callback_data="close")]
                 ]
             )
         )
@@ -27,10 +31,3 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         except:
             pass
 
-
-
-
-
-# MyselfNeon
-# Don't Remove Credit 🥺
-# Telegram Channel @NeonFiles
