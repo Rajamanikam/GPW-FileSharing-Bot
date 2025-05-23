5b6788b73ca0b10586da72320f2cd6814861d733
Rajamanikam <52708424+Rajamanikam@users.noreply.github.com>
2025-05-23 17:57:07 +0530

    Update1 config.py

--- config.py
+++ config.py
@@ -2,74 +2,47 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-
-
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
-
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = 7112455859
 DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "MyselfNeon")
+DB_NAME = "genieprojectsworld"
 
+CHANNEL_ID = -1002236200390
+FORCE_SUB_CHANNEL = -1002178375744
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002384933640"))
-
-
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "300")) # auto delete in seconds
-
+FILE_AUTO_DELETE = 0  # 0 means auto-delete is turned off
 
 PORT = os.environ.get("PORT", "8080")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-
-
 try:
-    ADMINS=[6848088376]
-    for x in (os.environ.get("ADMINS", "841851780").split()):
-        ADMINS.append(int(x))
+    ADMINS = [7112455859]  # Your Telegram ID as both admin and owner
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-
-
-
-
-
-
-
-
+    raise Exception("Your Admins list does not contain valid integers.")
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
 DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
 
-BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
+BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime>"
 
+USER_REPLY_TEXT = "<b><i>Please don't Send Me Messages Directly. I am just a File Sharing Bot 😄</i></b>"
 
+START_MSG = os.environ.get("START_MESSAGE",
+    "<b><i>Hi {mention} 👋\n\nWelcome to Genie Projects World Bot 🌐!\nI'm your permanent file store assistant.\nUse proper links shared in our channel to access your project files instantly.\n\nPowered by <a href='https://t.me/genieprojectsworld'>Genie Projects World</a></i></b>"
+)
 
-
-
-
-
-USER_REPLY_TEXT = "<b><i>Please don't Send Me Messages Directly I am just File Sharing Bot 😄</i></b>"
-
-START_MSG = os.environ.get("START_MESSAGE", "<b><i>Hello {mention}\n\nI am Permanent File Store Bot.\nDeveloped by <a href='https://t.me/MyselfNeon'>NeonAnurag</a>.\n\nGet Redirected from Correct Links to get the Files 🖇️</i></b>")
-
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b><i>Hello {mention}\n\nYou Need To Join In My Channel/Group To Use Me\n\n</i></b>")
-
-
-
-
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE",
+    "<b><i>Hello {mention} 🌟\n\nTo access project files, please join our official channel first:\n<a href='https://t.me/genieprojectsworld_offl'>Genie Projects World</a>\n\nIt's free, and it helps you stay updated with our latest projects! 🚀</i></b>"
+)
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(841851780)
 
-LOG_FILE_NAME = "filesharingbot.txt"
+LOG_FILE_NAME = "gpwfilesharingbot.txt"
 
 logging.basicConfig(
     level=logging.INFO,
@@ -86,15 +59,5 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-   
-
-
-
-
-
-# MyselfNeon
-# Don't Remove Credit 🥺
-# Telegram Channel @NeonFiles
